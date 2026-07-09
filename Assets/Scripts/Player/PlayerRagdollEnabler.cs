9f0b9ddafd3606440e9bcd69b528941edec8427c
Nicfrey <nicolas.fer9@gmail.com>
2026-07-09 16:35:25 +0200

    fix: ragdoll enabler when dying

--- Assets/Scripts/Player/PlayerRagdollEnabler.cs
+++ Assets/Scripts/Player/PlayerRagdollEnabler.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 public class PlayerRagdollEnabler : MonoBehaviour
 {
@@ -13,52 +15,96 @@ public class PlayerRagdollEnabler : MonoBehaviour
     [SerializeField] 
     private bool ragdollOnStart;
     private Collider[] ragdolColliders;
+    private Rigidbody[] ragdolRigidbodies;
     private Target health;
-    private Rigidbody rbCopy;
+    
+    private void Awake()
+    {
+        health = GetComponentInParent<Target>();
+        health.onDeath.AddListener(OnDeath);
+        health.onRevive.AddListener(OnRevive);
+        ragdolColliders = GetComponentsInChildren<Collider>();
+        ragdolRigidbodies = GetComponentsInChildren<Rigidbody>();
+        
+        RemovePunchColliders();
+    }
+    
+    private void RemovePunchColliders()
+    {
+        List<Collider> colliderList = new List<Collider>(ragdolColliders);
+        PlayerPunch[] punches = GetComponentsInChildren<PlayerPunch>();
+        foreach (var punch in punches)
+        {
+            colliderList.Remove(punch.GetComponent<Collider>());
+        }
+        ragdolColliders = colliderList.ToArray();
+    }
     
     // Start is called before the first frame update
     void Start()
     {
-        rbCopy = rb;
-        health = GetComponentInParent<Target>();
-        // health.onDeath.AddListener(OnDeath);
-        // health.onRevive.AddListener(OnRevive);
-        ragdolColliders = GetComponentsInChildren<Collider>();
-        if(!ragdollOnStart)
+        health.onDeath.AddListener(OnDeath);
+        health.onRevive.AddListener(OnRevive);
+        
+        SetRagdollEnabled(ragdollOnStart);
+    }
+
+    private void SetRagdollEnabled(bool enabled)
+    {
+        animator.enabled = !enabled;
+
+        foreach (Rigidbody rb in ragdolRigidbodies)
         {
-            foreach (Collider collider in ragdolColliders)
-            {
-                collider.isTrigger = true;
-            }
+            rb.isKinematic = !enabled;
+            rb.useGravity = enabled;
         }
-        else
+
+        foreach (Collider col in ragdolColliders)
         {
-            mainCollider.enabled = false;
-            animator.enabled = false;
-            rb.isKinematic = true;
+            col.enabled = enabled;
         }
     }
 
+    public void EnableRagdoll(Vector3 force, Vector3 hitPoint)
+    {
+        SetRagdollEnabled(true);
+        
+        Rigidbody nearestRb = GetNearestRigidbody(hitPoint);
+        if (nearestRb != null)
+        {
+            nearestRb.AddForce(force, ForceMode.Impulse);
+        }
+    }
+    
+    private Rigidbody GetNearestRigidbody(Vector3 point)
+    {
+        Rigidbody nearestRb = null;
+        float minDistance = float.MaxValue;
+    
+        foreach (Rigidbody rb in ragdolRigidbodies)
+        {
+            float distance = Vector3.Distance(rb.position, point);
+        
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                nearestRb = rb;
+            }
+        }
+    
+        return nearestRb;
+    }
+    
     private void OnRevive()
     {
-        foreach (Collider collider in ragdolColliders)
-        {
-            collider.isTrigger = true;
-        }
-        animator.enabled = true;
-        mainCollider.enabled = true;
-        rb.isKinematic = false;
+        SetRagdollEnabled(false);
     }
 
     private void OnDeath(PlayerStatistics arg0)
     {
-        foreach (Collider collider in ragdolColliders)
-        {
-            collider.isTrigger = false;
-        }
-        animator.enabled = false;
-        mainCollider.enabled = false;
-        rb.velocity = Vector3.zero;
-        rb.isKinematic = true;
+        var direction = Random.insideUnitSphere;
+        if(arg0 != null) 
+            direction = (transform.position - arg0.transform.position).normalized;
+        EnableRagdoll(direction * 5f, transform.position);
     }
 }
